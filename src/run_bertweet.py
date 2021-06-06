985c28db733e7a0d8948a64ee17e3acd96cae9c3
gary <han2114@columbia.edu>
2021-06-06 05:38:34 +0000

    finish bertweet

--- src/run_bertweet.py
+++ src/run_bertweet.py
@@ -1,14 +1,82 @@
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.model_selection import train_test_split
 import torch
-from utils import *
-from transformers import AutoTokenizer, AutoModel
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import torch.nn.functional as F
+from utils import *
+from transformers import AutoTokenizer, AutoModel, AutoConfig
 import random
 import time
-import torch.nn.functional as F
+
+def preprocessing_for_bert(data, tokenizer, MAX_LEN):
+    input_ids = []
+    attention_masks = []
+
+    # For every sentence...
+    for sent in data:
+        # tokenizer.encode_plus(): Tokenize and prepare for the model 
+        # a sequence or a pair of sequences.
+        encoded_sent = tokenizer.encode_plus(
+            text=sent,                        # Preprocess sentence
+            add_special_tokens=True,          # Add `[CLS]` and `[SEP]`
+            max_length=MAX_LEN,               # Max length to truncate/pad
+            pad_to_max_length=True,           # Pad sentence to max length
+            # return_tensors='pt',            # Return PyTorch tensor
+            truncation = True,                # Truncate to maximum length
+            return_attention_mask=True        # Return attention mask
+            )
+        input_ids.append(encoded_sent.get('input_ids'))
+        attention_masks.append(encoded_sent.get('attention_mask'))
+
+    # Convert lists to tensors
+    input_ids = torch.tensor(input_ids)
+    attention_masks = torch.tensor(attention_masks)
+
+    return input_ids, attention_masks
+
+class BertweetClassifier(nn.Module):
+    def __init__(self, freeze_bert=False):
+        super(BertweetClassifier, self).__init__()
+        
+        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
+        D_in, H, D_out = 768, 50, 2
+
+        # Instantiate BERT model
+        self.bertweet = AutoModel.from_pretrained("vinai/bertweet-base")
+        # self.LSTM = nn.LSTM(D_in,D_in,bidirectional=True)
+        # self.clf = nn.Linear(D_in*2,2)
+
+        # Instantiate an one-layer feed-forward classifier
+        
+        '''
+        self.classifier = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            nn.Linear(H, D_out)
+        )
+        '''
+
+        self.dropout = nn.Dropout(p=0.1)
+        self.classifier = nn.Linear(D_in, D_out)
+
+        # Freeze the BERT model
+        if freeze_bert:
+            for param in self.bertweet.parameters():
+                param.requires_grad = False
+        
+    def forward(self, input_ids, attention_mask):
+        # Feed input to BERT
+        outputs = self.bertweet(input_ids=input_ids,
+                                attention_mask=attention_mask)
+        # Extract the last hidden state of the token `[CLS]` for classification task
+        last_hidden_state_cls = outputs[0][:, 0, :]
+        hidden_state_dropout = self.dropout(last_hidden_state_cls)
+        # Feed input to classifier to compute logits
+        logits = self.classifier(hidden_state_dropout)
+
+        return logits
 
 def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=False):
     """Train the BertClassifier model.
@@ -131,14 +199,38 @@ def evaluate(model, val_dataloader):
 
     return val_loss, val_accuracy
 
+def initialize_model(epochs=4):
+    """Initialize the Bert Classifier, the optimizer and the learning rate scheduler.
+    """
+    # Instantiate Bert Classifier
+    bertweet_classifier = BertweetClassifier(freeze_bert=False)
+
+    # Tell PyTorch to run the model on GPU
+    bertweet_classifier.to(device)
+
+    # Create the optimizer
+    optimizer = AdamW(bertweet_classifier.parameters(),
+                      lr=5e-5,    # Default learning rate
+                      eps=1e-8    # Default epsilon value
+                      )
+
+    # Total number of training steps
+    total_steps = len(train_dataloader) * epochs
+
+    # Set up the learning rate scheduler
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0, # Default value
+                                                num_training_steps=total_steps)
+    return bertweet_classifier, optimizer, scheduler
+
 if __name__ == '__main__':
 
     config = load_config('config.yaml')
+    train_df = pd.read_csv(config['train_path'])
 
-    train = pd.read_csv(config['train_path'])
-    
-    all_train_texts = train.text.to_list()
-    all_train_labels = train.target.to_list()
+    # Train-test split
+    all_train_texts = train_df.text.to_list()
+    all_train_labels = train_df.target.to_list()
 
     X_train, X_val, y_train, y_val = train_test_split(
         all_train_texts, all_train_labels, 
@@ -146,20 +238,22 @@ if __name__ == '__main__':
         random_state=42
     )
 
-    tokenizer = AutoTokenizer.from_pretrained("vinai/bertweet-base", use_fast=False)
+    # Specify model name
+    model_name = "vinai/bertweet-base"
 
-    MAX_LEN = 250
-
+    # Tokenizing data
+    max_len = config['max_len']
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
     print('Tokenizing data...')
-    train_inputs, train_masks = preprocessing_for_bert(X_train, tokenizer, MAX_LEN)
-    val_inputs, val_masks = preprocessing_for_bert(X_val, tokenizer, MAX_LEN)
+    train_inputs, train_masks = preprocessing_for_bert(X_train, tokenizer, max_len)
+    val_inputs, val_masks = preprocessing_for_bert(X_val, tokenizer, max_len)
 
     # Convert other data types to torch.Tensor
     train_labels = torch.tensor(y_train)
     val_labels = torch.tensor(y_val)
 
     # For fine-tuning BERT, the authors recommend a batch size of 16 or 32.
-    batch_size = 12
+    batch_size = config['training_batch_size']
 
     train_data = TensorDataset(train_inputs, train_masks, train_labels)
     train_sampler = RandomSampler(train_data)
@@ -169,14 +263,39 @@ if __name__ == '__main__':
     val_sampler = SequentialSampler(val_data)
     val_dataloader = DataLoader(val_data, sampler=val_sampler, batch_size=batch_size)
 
-    bertweet = AutoModel.from_pretrained("vinai/bertweet-base")
+    # Define model
+    bertweet = AutoModel.from_pretrained(model_name)
 
     # Specify loss function
     loss_fn = nn.CrossEntropyLoss()
+
+    if torch.cuda.is_available():       
+        device = torch.device("cuda")
+        print(f'There are {torch.cuda.device_count()} GPU(s) available.')
+        print('Device name:', torch.cuda.get_device_name(0))
+
+    else:
+        print('No GPU available, using the CPU instead.')
+        device = torch.device("cpu")
     
-    set_seed(42)    
-    bertweet_classifier, optimizer, scheduler = initialize_model(epochs=2)
+    set_cuda_seed(42)    
+    bertweet_classifier, optimizer, scheduler = initialize_model(epochs=3)
     train(bertweet_classifier, train_dataloader, val_dataloader, epochs=3, evaluation=True)
+
+    # Save Local
+    bert_config = AutoConfig.from_pretrained(model_name)
+
+    model_path = '../models/bertweet'
+    tokenizer_path = os.path.join(model_path, 'tokenizer')
+    bertweet_classifier.save_pretrained(model_path)
+    tokenizer.save_pretrained(tokenizer_path)
+    bert_config.save_pretrained(tokenizer_path)
+
+    # Save Huggingface Hub
+    hub_model_path = 'disaster_tweet_bertweet'
+    bertweet_classifier.push_to_hub(hub_model_path)
+    tokenizer.push_to_hub(hub_model_path)
+    bert_config.push_to_hub(hub_model_path)
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import torch
 import yaml
 import torch
 import torch.nn as nn
-from transformers import BertModel
+from transformers import BertModel, AutoModel
 from transformers import AdamW, get_linear_schedule_with_warmup
 
 def compute_metrics(pred):
@@ -71,95 +71,3 @@ def load_config(config_path):
     with open(config_path) as f:
         config = yaml.safe_load(f)
     return config
-
-def preprocessing_for_bert(data, tokenizer, MAX_LEN):
-    input_ids = []
-    attention_masks = []
-
-    # For every sentence...
-    for sent in data:
-        # tokenizer.encode_plus(): Tokenize and prepare for the model 
-        # a sequence or a pair of sequences.
-        encoded_sent = tokenizer.encode_plus(
-            text=sent,    # Preprocess sentence
-            add_special_tokens=True,          # Add `[CLS]` and `[SEP]`
-            max_length=MAX_LEN,               # Max length to truncate/pad
-            pad_to_max_length=True,           # Pad sentence to max length
-            # return_tensors='pt',            # Return PyTorch tensor
-            truncation = True,                # Truncate to maximum length
-            return_attention_mask=True        # Return attention mask
-            )
-        input_ids.append(encoded_sent.get('input_ids'))
-        attention_masks.append(encoded_sent.get('attention_mask'))
-
-    # Convert lists to tensors
-    input_ids = torch.tensor(input_ids)
-    attention_masks = torch.tensor(attention_masks)
-
-    return input_ids, attention_masks
-
-class BertweetClassifier(nn.Module):
-    def __init__(self, freeze_bert=False):
-        super(BertweetClassifier, self).__init__()
-        
-        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
-        D_in, H, D_out = 768, 50, 2
-
-        # Instantiate BERT model
-        self.bertweet = AutoModel.from_pretrained("vinai/bertweet-base")
-        # self.LSTM = nn.LSTM(D_in,D_in,bidirectional=True)
-        # self.clf = nn.Linear(D_in*2,2)
-
-        # Instantiate an one-layer feed-forward classifier
-        
-        '''
-        self.classifier = nn.Sequential(
-            nn.Linear(D_in, H),
-            nn.ReLU(),
-            nn.Linear(H, D_out)
-        )
-        '''
-
-        self.dropout = nn.Dropout(p=0.1)
-        self.classifier = nn.Linear(D_in, D_out)
-
-        # Freeze the BERT model
-        if freeze_bert:
-            for param in self.bertweet.parameters():
-                param.requires_grad = False
-        
-    def forward(self, input_ids, attention_mask):
-        # Feed input to BERT
-        outputs = self.bertweet(input_ids=input_ids,
-                                attention_mask=attention_mask)
-        # Extract the last hidden state of the token `[CLS]` for classification task
-        last_hidden_state_cls = outputs[0][:, 0, :]
-        hidden_state_dropout = self.dropout(last_hidden_state_cls)
-        # Feed input to classifier to compute logits
-        logits = self.classifier(hidden_state_dropout)
-
-        return logits
-
-def initialize_model(epochs=4):
-    """Initialize the Bert Classifier, the optimizer and the learning rate scheduler.
-    """
-    # Instantiate Bert Classifier
-    bertweet_classifier = BertweetClassifier(freeze_bert=False)
-
-    # Tell PyTorch to run the model on GPU
-    bertweet_classifier.to(device)
-
-    # Create the optimizer
-    optimizer = AdamW(bertweet_classifier.parameters(),
-                      lr=5e-5,    # Default learning rate
-                      eps=1e-8    # Default epsilon value
-                      )
-
-    # Total number of training steps
-    total_steps = len(train_dataloader) * epochs
-
-    # Set up the learning rate scheduler
-    scheduler = get_linear_schedule_with_warmup(optimizer,
-                                                num_warmup_steps=0, # Default value
-                                                num_training_steps=total_steps)
-    return bertweet_classifier, optimizer, scheduler
